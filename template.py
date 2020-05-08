185efef60a75509c3ff063d7363072675774dd13
Starky <david990917@qq.com>
2020-05-08 22:12:02 +0800

    上传 template

---
+++ template.py
@@ -0,0 +1,49 @@
+README_CN = """
+| [English](README_EN.md) | 简体中文 |
+
+<p align="center"><img width="300" src="https://github.com/david990917/LeetCode-CN-Crawler/blob/master/image/leetcode-logo.png"></p>
+<p align="center">
+    <img src="https://img.shields.io/badge/用户-{user_name}-blue.svg?" alt="">
+    <img src="https://img.shields.io/badge/已解决-{num_solved}/{num_total}-blue.svg?" alt="">
+    <img src="https://img.shields.io/badge/简单-{ac_easy}-green.svg?" alt="">
+    <img src="https://img.shields.io/badge/中等-{ac_medium}-orange.svg?" alt="">
+    <img src="https://img.shields.io/badge/困难-{ac_hard}-red.svg?" alt="">
+</p>
+<h1 align="center">My LeetCode Solutions</h1>
+
+<p align="center">
+    <br>
+    <b>最近一次更新: {time}</b>
+    <br>
+</p>
+<p align="center">This repo is powered by <a href="https://github.com/david990917/LeetCode-CN-Crawler">LeetCode-CN-Crawler</a>.</p>
+
+| #    | 题目 | 难度 | 标签 |
+| :----: | :---- | :----: | :----: |
+"""
+
+README_EN = """
+| English | [简体中文](README.md) |
+
+<p align="center"><img width="300" src="https://github.com/david990917/LeetCode-CN-Crawler/blob/master/image/leetcode-logo.png"></p>
+<p align="center">
+    <img src="https://img.shields.io/badge/User-{user_name}-blue.svg?" alt="">
+    <img src="https://img.shields.io/badge/Solved-{num_solved}/{num_total}-blue.svg?" alt="">
+    <img src="https://img.shields.io/badge/Easy-{ac_easy}-green.svg?" alt="">
+    <img src="https://img.shields.io/badge/Medium-{ac_medium}-orange.svg?" alt="">
+    <img src="https://img.shields.io/badge/Hard-{ac_hard}-red.svg?" alt="">
+</p>
+<h1 align="center">My LeetCode Solutions</h1>
+
+<p align="center">
+    <br>
+    <b>最近一次更新: {time}</b>
+    <br>
+</p>
+<p align="center">This repo is powered by <a href="https://github.com/david990917/LeetCode-CN-Crawler">LeetCode-CN-Crawler</a>.</p>
+
+| #    | Title | Difficulty | Tags |
+| :----: | :----- | :----------: | :----: |
+"""
+
+readme_adding = "|[{frontend_id}]({formal_url})|[{title}]({githubURL})|{difficulty}|{tags}|\n"
